f2bf13a465b44287bda8b9cd57aaa5503529ac25
silviogodinho <silvio.godinho@platformbuilders.io>
2023-03-01 03:06:59 -0300

    update files gitactions

--- terraform-apply/main.tf
+++ terraform-apply/main.tf
@@ -1,6 +1,6 @@
 module "dynamo" {
 
-  source = "github.com/platformbuilders/builders-aws-dynamo-terraform-module"
+  source = "git::git@github.com:platformbuilders/builders-aws-dynamo-terraform-module.git"
 
   name           = "access_manager_test"
   hash_key       = "test"
